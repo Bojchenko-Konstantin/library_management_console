1c54cacc7f30a19497c5ce084865ce0b35c0fc06
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2024-11-20 22:05:55 +0500

    feat: Create Book model

---
+++ app/models/book.py
@@ -0,0 +1,28 @@
+class Book:
+    """Book model"""
+
+    def __init__(
+        self, id: int, title: str, author: str, year: int, status: str = "В наличии"
+    ) -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.status = status
+
+    def convert_instance_to_dict(self) -> dict:
+        """Convert instance of class 'Book' to dictionary for saving in JSON"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "status": self.status,
+        }
+
+    @staticmethod
+    def make_object_from_dict(data: dict) -> "Book":
+        """Create an instance of class 'Book' from dictionary"""
+        return Book(
+            data["id"], data["title"], data["author"], data["year"], data["status"]
+        )
